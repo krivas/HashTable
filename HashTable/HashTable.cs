f9ab48030c37842009ecbb79339bf6de6f63330d
KEVIN RIVAS <kevin_rivas@bhdleon.com.do>
2019-10-15 14:34:44 -0400

    uploading hashTable

---
+++ HashTable/HashTable.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HashTable
+{
+    public class HashTable
+    {
+        private LinkedList<string>[]   Table { get; set; }
+        public int Length { get; set; }
+
+        private List<string> Keys { get; set; }
+        public HashTable()
+        {
+            Table = new LinkedList<string>[3];
+            for (int i = 0; i < Table.Length; i++)
+                Table[i] = new LinkedList<string>();
+            Keys = new List<string>();
+        }
+        public void Add(string key, string value)
+        {
+            
+            var index=HashFunction(key, Table);
+            Table[index].AddLast(value);
+            Length = Length + 1;
+            Keys.Add(key);
+        }
+
+        public void Print()
+        {
+            var elementsWithValues= new List<int>();
+            foreach (var key in Keys)
+            {
+                var index=HashFunction(key, Table);
+                if (!elementsWithValues.Contains(index))
+                    elementsWithValues.Add(index);
+
+            }
+
+            foreach (var index in elementsWithValues)
+            {
+                Console.Write("{0} :", index);
+                foreach (var item in Table[index])
+                    Console.Write("{0} ",item);
+
+                Console.WriteLine();
+            }
+            
+                
+            
+        }
+
+        public void CopyHashMap()
+        {
+
+        }
+
+        static int HashFunction(string s, LinkedList<string>[] array)
+        {
+            int total = 0;
+            char[] c;
+            c = s.ToCharArray();
+
+            // Summing up all the ASCII values  
+            // of each alphabet in the string 
+            for (int k = 0; k <= c.GetUpperBound(0); k++)
+                total += (int)c[k];
+
+            return total % array.GetUpperBound(0);
+        }
+      
+    }
+}
---
+++ HashTable/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace HashTable
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var hashTable = new HashTable();
+           // var list = new string[3];
+            hashTable.Add("a", "hola");
+            hashTable.Add("bcfghfhfgh;klkjlijljlkjlkjlkjlkjkljlbcfghfhfgh;klkjlijljlkjlkjlkjlkjkljl", "adios");
+            //hashTable.Add("c", "kevin");
+            //hashTable.Add("d", "rivas");
+            hashTable.Print();
+
+            //Console.WriteLine(HashFunction("a", list));
+            //Console.WriteLine(HashFunction("rivas", list));
+            //Console.WriteLine(HashFunction("bcfghfhfgh", list));
+            
+        }
+
+     
+    }
+}
